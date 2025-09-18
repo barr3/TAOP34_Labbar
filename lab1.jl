c5df82e8081faff83e65e92d038a1b00a39646c5
Barre Blomberg <fred03.blomberg@gmail.com>
2025-09-18 11:46:06 +0200

    Micke gjorde knas

--- lab1.jl
+++ lab1.jl
@@ -1,52 +1,39 @@
 using JuMP, HiGHS
 
 m = 10
-N = 6
 L = 100
 M = 3
-#total_lenght = L*x
-#find minimal value of x that satisfies the demand and minimize waste.
 l = [11 13 17 23 37 41 51 61 71 79]
 
+iterMax = 20
+reducedCostThreshold = -1e-8
 
-A = [0 0 0 0 0 0 0 0 0 1   
-     0 1 1 0 0 0 0 0 0 0
-     0 0 0 1 1 0 0 1 0 0
-     0 0 0 0 0 0 0 0 1 0
-     0 0 0 0 0 1 1 0 0 0
-     1 0 0 0 0 0 0 0 0 0]
+A = [0 0 0 0 0 1
+     0 1 0 0 0 0
+     0 1 0 0 0 0
+     0 0 1 0 0 0
+     0 0 1 0 0 0
+     0 0 0 0 1 0
+     0 0 0 0 1 0
+     0 0 0 0 0 1
+     0 0 0 1 0 0
+     1 0 0 0 0 0]
 
-A = transpose(A)
 
 b = ones(1,m) # Demand
-c = ones(1,N)
 
 
-model = Model(HiGHS.Optimizer)
-@variable(model, x[1:N] >= 0)
+pattern_len(col) = sum(l[j] for j in 1:m if col[j] > 1e-9)
+pattern_pieces(col) = sum(col .> 1e-9)
+pattern_waste(col) = L - pattern_len(col)
 
-demand = @constraint(model, A*x .== b)
-
-@objective(model, Min, sum(x))
-
-
-optimize!(model)
-obj = objective_value(model)
-x_opt = value.(x)
-u = dual.(demand)
-
-println("LP-optimum (sum x) = ", obj)
-println("x* = ", x_opt)
-println("dualer u = ", u)
-
-
-function columnGeneration(dualVar)
-    print("\n", dualVar)
+function columnGeneration(V)
+    print("\n", V)
     columnModel = Model(HiGHS.Optimizer)
     @variable(columnModel, a[1:m]>=0, Bin)
     @constraint(columnModel, column, sum(l[j] * a[j] for j in 1:m) <= L)
     @constraint(columnModel, max_cut , sum(a[j] for j in 1:m) <= M)
-    @objective(columnModel, Max, sum(dualVar[i] * a[i] for i in 1:m))
+    @objective(columnModel, Max, sum(V[i] * a[i] for i in 1:m))
     print("\nColumn Generation!\n")
     optimize!(columnModel)
     println("\n", value.(a))
@@ -54,24 +41,41 @@ function columnGeneration(dualVar)
 end
 
 
-y = columnGeneration(u)
-# A = hcat(A, x)
 
+for i in 1:iterMax
+    N = size(A,2)
+    LP = Model(HiGHS.Optimizer)
 
+    @variable(LP, x[1:N]>=0)
+    @constraint(LP, demand[j in 1:m], sum( A[j,i]*x[i] for i in 1:N ) >= b[j])
+    @constraint(LP, maximum[i in 1:N], sum( A[j,i]*x[i] for j in 1:m ) <= M )
 
-# print(transpose(A))
+    @objective(LP, Min, sum(x) )
 
-function reducedCost(c, y, A)
-    c_bar = c - transpose(y) * A
-    return c_bar
+    optimize!(LP)
+    V = [dual(demand[i]) for i in 1:m]
+    column = columnGeneration(V)
+    c_bar = 1 - V' * column
+
+    if c_bar >= reducedCostThreshold
+        break
+    end
+    global A = [A column]
+    print(size(A))
 end
 
-println(reducedCost(c, y, A))
 
+N = size(A,2)
+LP = Model(HiGHS.Optimizer)
+@variable(LP, x[1:N], Bin)
+@constraint(LP, demand[j in 1:m], sum( A[j,i]*x[i] for i in 1:N ) .== b[j])
+@constraint(LP, maximum[i in 1:N], sum( A[j,i]*x[i] for j in 1:m ) <= M )
+@objective(LP, Min, sum(x) )
+optimize!(LP)
 
-
-
-
+print("\nx values:\n", value.(x), "\n")
+print("Objective value: ", objective_value(LP), "\n")
+print("\n\nA: ", A)
 
 
 
